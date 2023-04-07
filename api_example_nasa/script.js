9d1a19670b0181463e80293742d1edaee60d30e0
Lai Yi Ohlsen <laiyi.ohlsen@gmail.com>
2023-04-07 07:21:53 -0400

    updating files

---
+++ api_example_nasa/script.js
@@ -0,0 +1,23 @@
+const key = "U0pZbteybkYkIiWehVdcp03hYj92BvSWeb5UMul5"
+
+const api_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key="+key+""
+async function getMars() {
+    
+      // Making an API call (request)
+      // and getting the response back
+      const response = await fetch(api_url);
+    
+      // Parsing it to JSON format
+      const data = await response.json();
+      
+      console.log(data)
+
+      let url = data.photos[0].img_src;
+
+      document.getElementById("image").src=url;
+}
+    
+    // Calling the function
+getMars();
+
+
